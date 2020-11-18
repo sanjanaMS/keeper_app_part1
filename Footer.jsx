4f446f2c90a5726f571f114f96ec777bd27f647d
sanjanaMS <56424429+sanjanaMS@users.noreply.github.com>
2020-11-18 09:06:05 -0800

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    const currentDate=new Date();
+    const year=currentDate.getFullYear();
+    return <footer>
+        <p>Copyright {year}</p>
+    </footer>;
+}
+export default Footer;
